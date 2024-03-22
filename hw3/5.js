059421be0c73eb0404cf804673ee8ad10338b118
roman lepihov <lepihovrom@yandex.by>
2024-03-22 13:26:05 +0300

    add 5

---
+++ hw3/5.js
@@ -0,0 +1,23 @@
+// Реализовать функцию round5, которой передается целое число, функция должна
+// возвращать ближайшее целое число к тому что было передано, без остатка делящееся
+// на 5.
+//
+//   ```js
+function round5(val) {
+  const remainder = val % 5;
+
+  if (Math.abs(remainder) < 2.5) {
+    return val - remainder;
+  } else {
+    return val + ((val >= 0 ? 5: -5) - remainder);
+  }
+}
+
+console.log(round5(0)); // 0
+console.log(round5(2)); // 0
+console.log(round5(3)); // 5
+console.log(round5(11)); // 10
+console.log(round5(14)); // 15
+console.log(round5(50)); // 50
+console.log(round5(-2)); // 0
+console.log(round5(-3)); // -5
